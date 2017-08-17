9a7a390d66083fff2738ec7e64b0258a3ca2675c
zhangyw <zhangyw001@gmail.com>
2017-08-17 13:50:55 +0800

    修改了读取文件的方式为 class.getResources 这种，之前的方式会读不到文件

--- src/main/java/com/zhangyingwei/miner/service/selector/MinerSelector.java
+++ src/main/java/com/zhangyingwei/miner/service/selector/MinerSelector.java
@@ -1,6 +1,7 @@
 package com.zhangyingwei.miner.service.selector;
 
 import com.zhangyingwei.miner.service.selector.model.Selector;
+import org.apache.log4j.Logger;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -16,6 +17,7 @@ import java.util.Map;
  * Created by zhangyw on 2017/8/16.
  */
 public class MinerSelector {
+    private Logger logger = Logger.getLogger(MinerSelector.class);
     private String path;
     private Map<String, Selector> selectorMap;
 
@@ -24,18 +26,32 @@ public class MinerSelector {
         this.selectorMap = new HashMap<String, Selector>();
     }
 
-    private void read() throws DocumentException {
+    public MinerSelector read() throws DocumentException {
         SAXReader reader = new SAXReader();
+        this.path = this.getClass().getResource(this.path).getPath();
         Document doc = reader.read(new File(this.path));
         Element root = doc.getRootElement();
         root.elements(QName.get("selector")).forEach(sel -> {
             Selector selector = new Selector((Element) sel);
             selectorMap.put(selector.getKey(), selector);
         });
+        return this;
     }
 
     public Selector getSelector(String key) {
         String realKey = URI.create(key).getHost();
-        return this.selectorMap.get(realKey);
+        Selector value = this.selectorMap.get(realKey);
+        if(value == null){
+            logger.info("key - " + realKey +" is not found in selector map");
+        }
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "MinerSelector{" +
+                "path='" + path + '\'' +
+                ", selectorMap=" + selectorMap +
+                '}';
     }
 }
