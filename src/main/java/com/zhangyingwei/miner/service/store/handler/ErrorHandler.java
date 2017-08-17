0b85bbe4ea5cc3df65a0361bbccb2e8055d4495e
zhangyw <zhangyw001@gmail.com>
2017-08-17 13:49:51 +0800

    修改了日志打印方式为 log4j

--- src/main/java/com/zhangyingwei/miner/service/store/handler/ErrorHandler.java
+++ src/main/java/com/zhangyingwei/miner/service/store/handler/ErrorHandler.java
@@ -5,12 +5,14 @@ import com.zhangyingwei.cockroach.http.handler.ITaskErrorHandler;
 import com.zhangyingwei.miner.service.date.action.ResourcesAction;
 import com.zhangyingwei.miner.service.exception.MinerException;
 import com.zhangyingwei.miner.service.store.GroupEnum;
+import org.apache.log4j.Logger;
 
 /**
  * Created by zhangyw on 2017/8/16.
  */
 public class ErrorHandler implements ITaskErrorHandler {
 
+    private Logger logger = Logger.getLogger(ErrorHandler.class);
     private ResourcesAction resourcesAction;
 
     public ErrorHandler() {
@@ -19,11 +21,11 @@ public class ErrorHandler implements ITaskErrorHandler {
 
     public void error(Task task, String message) {
         try {
-            System.out.println("ERROR-TASK:"+task);
+            logger.error("ERROR-TASK:"+task);
             if(GroupEnum.RSS.getName().equals(task.getGroup()) || GroupEnum.SITE.getName().equals(task.getGroup())){
                 this.resourcesAction.unvalid(task);
             }
-            System.out.println("ERROR: "+task.getUrl());
+            logger.error(""+task.getUrl());
         } catch (MinerException e) {
 //            System.out.println(e.getMessage());
         }
